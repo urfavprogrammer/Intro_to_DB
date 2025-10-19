8482abc90ff82f7f1909f73656b76f47329767d1
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-10-19 11:46:53 -0700

    simple python script that creates the databse alx_book_store

---
+++ MySQLServer.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    connection = None
+    try:
+        # Establish connection to MySQL server
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="admin",
+            password="Chief042$$"  # Replace with your MySQL password
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            
+            # Create database
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+
+    finally:
+        # Close database connection
+        if connection and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+if __name__ == "__main__":
+    create_database()
